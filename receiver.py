356c4c866efd9acbeec6acddeb396a5eb4d85aa7
Joao Zsigmond <jzsiggy@gmail.com>
2020-11-02 16:45:35 -0300

    Adding receiver file

---
+++ receiver.py
@@ -0,0 +1,45 @@
+from matplotlib import pyplot as plt
+import sounddevice as sd
+import numpy as np
+from scipy.fftpack import fft, fftshift
+
+class Receiver:
+    def __init__(self, fs):
+        self.fs = fs
+
+    def record(self, seconds):
+        duration = seconds
+        self.recording = sd.rec(int(duration * self.fs), samplerate=self.fs, channels=1)
+        sd.wait()
+
+    def plot_recording(self):
+        plt.plot(self.recording)
+        plt.grid()
+        plt.show()
+
+    def play_recording(self):
+        sd.play(self.recording, self.fs)
+        sd.wait()
+
+    def calcFFT(self):
+        signal = self.recording * 100
+        fs = self.fs
+        
+        N  = len(signal)
+        T  = 1/fs
+        xf = np.linspace(-1.0/(2.0*T), 1.0/(2.0*T), N)
+        yf = fft(signal)
+        X, Y = xf, fftshift(yf)
+
+        plt.figure()
+        plt.plot(X, np.abs(Y))
+        plt.grid()
+        plt.xlim(0, 10000)
+        plt.show()
+
+
+receiver = Receiver(48000)
+receiver.record(3)
+receiver.play_recording()
+receiver.plot_recording()
+receiver.calcFFT()
