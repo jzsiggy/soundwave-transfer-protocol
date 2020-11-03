55c17531f9d55a24f8d5fdb1544491e2d48f46a8
Joao Zsigmond <jzsiggy@gmail.com>
2020-11-02 22:22:08 -0300

    Adding hashmap to Receiver

--- receiver.py
+++ receiver.py
@@ -10,6 +10,18 @@ class Receiver:
     def __init__(self, fs, recording=None):
         self.fs = fs
         self.recording = recording
+        self.hash = {
+            0 : [941, 1336],
+            1 : [697, 1209],
+            2 : [697, 1336],
+            3 : [697, 1477],
+            4 : [770, 1209],
+            5 : [770, 1336],
+            6 : [770, 1477],
+            7 : [852, 1209],
+            8 : [852, 1336],
+            9 : [852, 1477],
+        }
 
     def record(self, seconds):
         duration = seconds
